8e2765b168643e40dde64775a68e10ec932d8c2a
AreejAbumuhfouz <areejabumahfouz@gmail.com>
2025-08-23 22:33:39 +0300

    23/8/2025

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -1,15 +1,32 @@
+// require('dotenv').config();
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.DB_HOST,
+//         dialect: 'postgres',
+//         port: process.env.DB_PORT,
+//     }
+// );
+
+// module.exports = sequelize;
+
+
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        port: process.env.DB_PORT,
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,  // Needed for Render PostgreSQL SSL
+    },
+  },
+});
 
 module.exports = sequelize;
