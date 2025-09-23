924f369f0e2539dcb79441fe6f997eb6d3505f43
AreejAbumuhfouz <areejabumahfouz@gmail.com>
2025-09-23 11:21:07 +0300

    23/9/2025

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -15,18 +15,36 @@
 // module.exports = sequelize;
 
 
+// require('dotenv').config();
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   protocol: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,  // Needed for Render PostgreSQL SSL
+//     },
+//   },
+// });
+
+// module.exports = sequelize;
+
+
+
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,  // Needed for Render PostgreSQL SSL
+      rejectUnauthorized: false, // Neon requires SSL
     },
   },
 });
 
 module.exports = sequelize;
+
