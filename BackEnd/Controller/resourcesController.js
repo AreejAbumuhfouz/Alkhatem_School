54c313657438778f3746a0de90be6aae11f9c7b3
Areej Abumuhfouz <areejabumahfouz@gmail.com>
2025-09-23 23:37:37 +0300

    23/9/2025

--- BackEnd/Controller/resourcesController.js
+++ BackEnd/Controller/resourcesController.js
@@ -160,10 +160,10 @@ exports.uploadResourcesFile = async (req, res) => {
     if (!req.file) return res.status(400).json({ error: "No file uploaded" });
 
     // قراءة Excel
-    const workbook = XLSX.read(req.file.buffer, { type: "buffer" });
+    const workbook = xlsx.read(req.file.buffer, { type: "buffer" });
     const sheetName = workbook.SheetNames[0];
     const sheet = workbook.Sheets[sheetName];
-    const data = XLSX.utils.sheet_to_json(sheet);
+    const data = xlsx.utils.sheet_to_json(sheet);
 
     const createdResources = [];
 
