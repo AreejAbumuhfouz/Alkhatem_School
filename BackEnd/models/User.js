e59c949718869062ba8ff15a7780707392554146
Areej Abumuhfouz <areejabumahfouz@gmail.com>
2025-10-12 20:39:42 +0300

    12/10

--- BackEnd/models/User.js
+++ BackEnd/models/User.js
@@ -15,10 +15,13 @@ const User = sequelize.define('User', {
         allowNull: false,
     },
     email: {
-        type: DataTypes.STRING(150),
-        allowNull: false,
-        unique: true,
-    },
+  type: DataTypes.CITEXT, // البريد الإلكتروني غير حساس لحالة الحروف
+  allowNull: false,
+  unique: true,
+  validate: {
+    isEmail: true,
+  },
+},
     password: {
         type: DataTypes.STRING(255),
         allowNull: false,
