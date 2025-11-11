9081ef27186d426de659a4ea852b9d39c4923b53
Areej Abumuhfouz <areejabumahfouz@gmail.com>
2025-11-11 19:23:39 +0300

    11/11/2025

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <- هذا مهم للـ SPA على Nginx
+  build: {
+    outDir: 'dist', // مكان ملفات البناء
+  },
 })
