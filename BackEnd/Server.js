40a2985e4ae73825f8f402c9a82afa7ca9eafed3
Areej Abumuhfouz <areejabumahfouz@gmail.com>
2025-11-16 14:39:55 +0300

    16/11

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -60,9 +60,12 @@ app.use(cookieParser());
 
 
 
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+    // app.listen(PORT, () => {
+    //   console.log(`Server running on http://localhost:${PORT}`);
+    // });
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
   } catch (error) {
     console.error('Unable to connect to the database:', error.message);
